e143fe5c152982f072ad089fd8c44f2198c62cb0
Ana Alves <anamarias.ufg@gmail.com>
2024-07-04 20:52:40 -0300

    Adição do módulo 3

--- modulo_3/exercicios.R
+++ modulo_3/exercicios.R
@@ -21,3 +21,31 @@ str(empregados)
 #Filtre as linhas onde o Salario é maior que 4000.
 empregados[c("Nome", "Salario")]
 empregados[ empregados$Salario > 4000, ]
+
+#Exercício 3: Adição e remoção de colunas. Adicione uma nova coluna Bonus que é 10% do Salario. Remova a coluna Cidade do 
+#dataframe.
+
+empregados <- subset(empregados, select = -Cidade)
+
+empregados$Bonus <- empregados$Salario * 0.1
+
+#Exercício 4: Ordenação de dados.Ordene o data frame pela coluna Salario em ordem decrescente.
+#Ordene o data frame pela coluna Idade em ordem crescente.
+
+empregados <- empregados[order(empregados$Idade), ] #ordem crescente
+empregados <- empregados[order(empregados$Salario, decreasing = TRUE), ] #ordem decrescente
+
+#Exercício 5: Agrupamento e resumo de dados. Crie um novo data frame com as colunas Departamento, Funcionario, Salario.
+# Adicione pelo menos 6 linhas de dados, onde cada Departamento tenha pelo menos 2 funcionários.
+#Calcule a média de Salario por Departamento.
+departamentos <- data.frame(
+  Departamento <- c("Comercial", "Administrativo", "Comercial", "Administrativo", "Comercial"),
+  Funcionario <- c("Ana", "Lucas", "Flávia", "Catarina", "José"),
+  Salario <- c(50000.00, 100000.00, 20000.00,  1400.00, 5000.00)
+)
+
+Comercial <- departamentos[ departamentos$Departamento == "Comercial", ]
+Administrativo <- departamentos[ departamentos$Departamento == "Administrativo", ]
+
+media_comercial <- sum(Comercial$Salario, na.rm = TRUE)/sum(!is.na(Comercial$Salario))
+media_adm <- sum(Administrativo$Salario, na.rm = TRUE)/sum(!is.na(Administrativo$Salario))
